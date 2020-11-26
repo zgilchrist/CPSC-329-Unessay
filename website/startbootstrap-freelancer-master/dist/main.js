7961aa6c2eedbd7363a9f780ef85daed4d9231d4
delarasham <56743249+delarasham@users.noreply.github.com>
2020-11-26 14:24:34 -0700

    Update main.js

--- website/startbootstrap-freelancer-master/dist/main.js
+++ website/startbootstrap-freelancer-master/dist/main.js
@@ -1,12 +1,12 @@
-var container = document.querySelector('#BobCube');
+var container1 = document.querySelector('#BobCube');
 cube1 = new ERNO.Cube();
 light = new Photon.Light(10,0,100);
-container.appendChild(cube1.domElement);
+container1.appendChild(cube1.domElement);
 
-var container = document.querySelector('#AliceCube');
+var container2 = document.querySelector('#AliceCube');
 cube2 = new ERNO.Cube();
 light = new Photon.Light(10,0,100);
-container.appendChild(cube2.domElement);
+container2.appendChild(cube2.domElement);
 
 var input1='';
 document.getElementById("Button0").onclick = function (){
@@ -14,6 +14,10 @@ document.getElementById("Button0").onclick = function (){
     console.log(input1);
 }
 document.getElementById("submitH").onclick=function(){
-    cube1.twist(input1);
-
+    // removing the cubes
+    container1.removeChild(cube1.domElement);
+    container2.removeChild(cube2.domElement);
+    //swaping the cubes
+    container1.appendChild(cube2.domElement);
+    container2.appendChild(cube1.domElement);
 }
